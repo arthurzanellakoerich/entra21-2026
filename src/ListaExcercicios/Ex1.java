cabc8febeae31488db601f43484e46fc7523096b
Arthur Zanella Koerich <arthurzanella10@gmail.com>
2026-05-10 08:46:55 -0300

    Organização de excercicios

--- src/ListaExcercicios/Ex1.java
+++
@@ -1,8 +0,0 @@
-package ListaExcercicios;
-
-public class Ex1 {
-    public static void main(String[] args) {
-        int x = 10;
-        System.out.printf("Valor de x: " +  x);
-    }
-}
---
+++ src/ListaExcercicios/Lista.java
@@ -0,0 +1,4 @@
+package ListaExcercicios;
+
+public class Lista {
+}
